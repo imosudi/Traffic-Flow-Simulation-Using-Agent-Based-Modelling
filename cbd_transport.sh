6adbf103b5987e2c2e686399bd5a1854e6c7755a
Mosudi I. O. <imosudi@gmail.com>
2025-04-23 20:51:35 +0100

    Traffic lights

--- cbd_transport.sh
+++ cbd_transport.sh
@@ -44,6 +44,6 @@ echo "Starting SUMO simulation..."
 cd cbd/ && \
 netconvert -c cbd.netccfg && \
 /usr/share/sumo/tools/randomTrips.py -n cbd.net.xml -o cbd.rou.xml -r cbd.trips.xml \
-    --prefix veh --end 3600 --validate --seed 42 \
+    --prefix veh --end 21600 --validate --seed 42 \
     --trip-attributes="departLane=\"best\" departSpeed=\"max\" departPos=\"random\"" && \
 sumo-gui -c cbd.sumocfg
